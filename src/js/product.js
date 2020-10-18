5423e6c2b4650ba702ba3b880ae7ca2937dd09c6
wugang-ad <1355161386@qq.com>
2020-10-18 17:58:59 +0800

    10.product

---
+++ src/js/product.js
@@ -0,0 +1,12 @@
+$('#moreA').hover(
+    function(){
+        $('.more').css("display" ,"block");
+    }
+)
+$('.more').hover(
+    function(){
+        $('.more').css("display" ,"block");
+    }, function(){
+        $('.more').css("display" ,"none");
+    }
+)
